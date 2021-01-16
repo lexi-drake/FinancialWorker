3ffb79c6947d3b8e0820f424f0bb2ae8eae26c53
alexa <alexa11drake@gmail.com>
2021-01-16 13:58:44 -0600

    more testing

--- Tests/Handlers/GetExpiredUsersQueryHandlerShould.cs
+++ Tests/Handlers/GetExpiredUsersQueryHandlerShould.cs
@@ -11,6 +11,41 @@ namespace Tests
 {
     public class GetExpiredUsersQueryHandlerShould
     {
-        // TODO (alexa): implement.
+        private Mock<IUserRespository> _repo;
+        private GetExpiredUsersQueryHandler _handler;
+
+        public GetExpiredUsersQueryHandlerShould()
+        {
+            _repo = new Mock<IUserRespository>();
+            _repo.Setup(x => x.GetUsersByLastLoggedInAsync(It.IsAny<DateTime>()))
+                .Returns(Task.FromResult(GetListOfUsers()));
+
+            _handler = new GetExpiredUsersQueryHandler(new Mock<ILogger<GetExpiredUsersQueryHandler>>().Object, _repo.Object);
+        }
+
+        [Fact]
+        public async Task ReturnsUserIds()
+        {
+            var query = new GetExpiredUsersQuery();
+
+            var userIds = await _handler.Handle(query, new CancellationToken());
+            Assert.NotEmpty(userIds);
+
+            _repo.Verify(x => x.GetUsersByLastLoggedInAsync(It.IsAny<DateTime>()), Times.Once);
+        }
+
+        private IEnumerable<User> GetListOfUsers()
+        {
+            var count = new Random().Next(1, 1000);
+            var transactions = new List<User>();
+            for (var i = 0; i < count; i++)
+            {
+                transactions.Add(new User()
+                {
+                    Id = Guid.NewGuid().ToString()
+                });
+            }
+            return transactions;
+        }
     }
 }
